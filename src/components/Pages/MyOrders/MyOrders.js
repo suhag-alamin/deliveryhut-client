065544269ba76cb2ac2d7e67445e0673dadc1c79
Suhag Al Amin <developersuhag@gmail.com>
2021-10-30 03:06:47 +0600

    my orders design

--- src/components/Pages/MyOrders/MyOrders.js
+++ src/components/Pages/MyOrders/MyOrders.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Col, Container, Row, Spinner } from "react-bootstrap";
 import useAuth from "../../../hooks/useAuth";
 import useDocumentTitle from "../../../hooks/useDocumentTitle";
+import OthersBanner from "../../Shared/OthersBanner/OthersBanner";
 import "./MyOrder.css";
+import SingleOrder from "./SingleOrder/SingleOrder";
 
 const MyOrders = () => {
   // dynamic title
@@ -10,25 +13,49 @@ const MyOrders = () => {
 
   // states
   const [orders, setOrders] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   const { user } = useAuth();
   const email = user?.email;
   const emails = { email };
   const values = Object.values(emails);
-  console.log(values);
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .post("https://morning-sierra-84457.herokuapp.com/orders/byEmail", values)
       .then((orders) => {
         setOrders(orders.data);
-        console.log(orders);
+        setIsLoading(false);
       });
   }, []);
 
+  // spinner
+  if (isLoading) {
+    return (
+      <div className="text-center py-4">
+        <Spinner animation="border" variant="" style={{ color: "#006d77" }} />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h3>this is my orders : {orders?.length}</h3>
-    </div>
+    <>
+      {/* banner  */}
+      <OthersBanner>Orders</OthersBanner>
+      <Container className="py-5 px-4 colored-bg text-white rounded-3 shadow my-4">
+        <h2 className="text-center fs-2 mb-5">
+          My <span style={{ color: "#ffddd2" }}>Orders</span>
+        </h2>
+        <Row className="g-4">
+          {orders.map((order) => (
+            <Col lg={6} key={order._id}>
+              <SingleOrder order={order}></SingleOrder>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
   );
 };
 
---
+++ src/components/Pages/MyOrders/SingleOrder/SingleOrder.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import "./SingleOrder.css";
+
+const SingleOrder = ({ order }) => {
+  const { serviceName, img, date, status } = order;
+  return (
+    <>
+      <div className="order-details-box d-flex gap-4 justify-content-between align-items-center shadow">
+        <div>
+          <img className="order-img img-fluid" src={img} alt="" />
+        </div>
+        <div className="order-details">
+          <h4 className="mt-3 mt-lg-0 serive-title">{serviceName}</h4>
+          <p>
+            Status: <b>{status}</b>
+          </p>
+          <p>
+            <b>{date}</b>
+          </p>
+        </div>
+        <div className="">
+          <Button className="deliveryhut-outline-bt" variant="outline-light">
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SingleOrder;
