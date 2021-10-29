37fc6629087003428c8a055e7483bc3dbab22e39
Suhag Al Amin <developersuhag@gmail.com>
2021-10-29 16:55:08 +0600

    serive design

--- src/components/Shared/SingleService/SingleService.js
+++ src/components/Shared/SingleService/SingleService.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
+import "./SingleService.css";
 
 const SingleService = ({ service }) => {
   const { title, subTitle, description, img } = service;
   return (
     <>
       <Card className="h-100 shadow">
-        <Card.Img variant="top" src={img} />
+        <div className="overflow-hidden service-img">
+          <Card.Img className="img-fluid" variant="top" src={img} />
+        </div>
         <Card.Body>
-          <h6 className="text-uppercase">{subTitle}</h6>
-          <Card.Title className="text-uppercase">{title}</Card.Title>
-          <Card.Text>{description.slice(0, 100)}...</Card.Text>
+          <h6 className="text-uppercase sub-title">{subTitle}</h6>
+          <Card.Title className="text-uppercase title">{title}</Card.Title>
+          <Card.Text className="description">
+            {description.slice(0, 100)}...
+          </Card.Text>
         </Card.Body>
         <Card.Footer className="bg-white border-0 text-center pb-3">
           <Button className="deliveryhut-btn" variant="">
